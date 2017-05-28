c09402ec5b1fe3e1c1df30bfced5b5e35f438ac1
Александр Подоплелов <alepod@gmail.com>
2017-05-29 01:57:05 +0300

    Aligning management scripts

--- run.sh
+++ run.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
-docker-machine create -d virtualbox --virtualbox-cpu-count 4 --virtualbox-memory 7096 dclou || true
-eval $(docker-machine env dclou)
+
+if command -v docker-machine &>/dev/null; then
+  #docker-machine create -d virtualbox --virtualbox-cpu-count 4 --virtualbox-memory 7096 dclou || true
+  #eval $(docker-machine env dclou)
+  if [ $(docker-machine env dclou >/dev/null 2>&1) ]; then
+    eval $(docker-machine env dclou)
+    IP="$(docker-machine ip dclou)"
+    echo "Runnung in Virtaul Machine dclou with ip:$IP"
+  else
+    echo "Runnung on the local station"
+  fi
+fi
+
 docker stack deploy -c docker-compose.yml dclou
 # List all images
 docker stack ps dclou
