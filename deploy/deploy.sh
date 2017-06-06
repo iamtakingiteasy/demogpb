b4e504f952b8ae3a095e3dbfb3d79606240dc207
Alexander Tumin <iamtakingiteasy@eientei.org>
2017-06-06 07:05:00 +0300

    Increase verbosity

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,7 +2,7 @@
 
 HOST="$1"
 
-mvn -q -s .travis.settings.xml -B package -DdeployDocker -DskipTests
+mvn -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn -s .travis.settings.xml -B package -DdeployDocker -DskipTests
 
 ssh user@$HOST 'ssh user@10.10.50.11 rm -rf demogpb'
 ssh user@$HOST 'ssh user@10.10.50.11 git clone https://github.com/dclou/demogpb'
