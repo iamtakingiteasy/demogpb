062540f04b2421b4ef3ee97360a2660f956079c0
max <maxim.nikitin.work@gmail.com>
2017-06-06 21:45:43 +0300

    disable cloud

--- clean.sh
+++ clean.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker stack rm dclou
 #docker images 'dcloudemo/*' --format '{{.ID}}' | xargs docker rmi -f
-docker images '*' --format '{{.ID}}' | xargs docker rmi -f
+#docker images '*' --format '{{.ID}}' | xargs docker rmi -f
