c003da73f3a4857daafe9eb40cf3c74ead9dd5c7
Александр Подоплелов <alepod@gmail.com>
2017-06-06 15:34:06 +0300

    Настройка swagger

--- order/src/main/java/org/dclou/example/demogpb/order/config/SecurityConfiguration.java
+++ order/src/main/java/org/dclou/example/demogpb/order/config/SecurityConfiguration.java
@@ -139,6 +139,21 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                         .anyRequest().authenticated();
     }
 
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+    	registry.addRedirectViewController("/documentation/v2/api-docs", "/v2/api-docs");
+        registry.addRedirectViewController("/documentation/configuration/ui", "/configuration/ui");
+        registry.addRedirectViewController("/documentation/configuration/security", "/configuration/security");
+        registry.addRedirectViewController("/documentation/swagger-resources", "/swagger-resources");
+        registry.addRedirectViewController("/documentation", "/documentation/swagger-ui.html");
+        registry.addRedirectViewController("/documentation/", "/documentation/swagger-ui.html");
+
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/documentation/**").addResourceLocations("classpath:/META-INF/resources/");
+    }
 
     private OAuth2AuthenticationProcessingFilter oAuth2AuthenticationProcessingFilter() {
         OAuth2AuthenticationProcessingFilter oAuth2AuthenticationProcessingFilter =
--- order/src/main/java/org/dclou/example/demogpb/order/config/SwaggerConfig.java
+++ order/src/main/java/org/dclou/example/demogpb/order/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package org.dclou.example.demogpb.order;
+package org.dclou.example.demogpb.order.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
