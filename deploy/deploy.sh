86287b66b6b64d2bf4506b0d207e38f844a8546b
Alexander Tumin <iamtakingiteasy@eientei.org>
2017-06-06 07:02:15 +0300

    Build optimization

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,7 +2,7 @@
 
 HOST="$1"
 
-mvn -s .travis.settings.xml -B package -DdeployDocker -DskipTests
+mvn -q -s .travis.settings.xml -B package -DdeployDocker -DskipTests
 
 ssh user@$HOST 'ssh user@10.10.50.11 rm -rf demogpb'
 ssh user@$HOST 'ssh user@10.10.50.11 git clone https://github.com/dclou/demogpb'
