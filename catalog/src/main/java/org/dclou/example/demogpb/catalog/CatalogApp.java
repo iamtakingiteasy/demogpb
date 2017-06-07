72e4c14beb9567ac38ee6b5805efbf954e94cd44
Alexander Tumin <iamtakingiteasy@eientei.org>
2017-06-07 13:09:11 +0300

    Hystrix dashboard

--- catalog/src/main/java/org/dclou/example/demogpb/catalog/CatalogApp.java
+++ catalog/src/main/java/org/dclou/example/demogpb/catalog/CatalogApp.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -12,6 +13,7 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 
 import javax.annotation.PostConstruct;
 
+@EnableHystrixDashboard
 @EnableCircuitBreaker
 @SpringCloudApplication
 @EnableFeignClients
--- customer/src/main/java/org/dclou/example/demogpb/customer/CustomerApp.java
+++ customer/src/main/java/org/dclou/example/demogpb/customer/CustomerApp.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -11,6 +12,7 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 
 import javax.annotation.PostConstruct;
 
+@EnableHystrixDashboard
 @EnableCircuitBreaker
 @SpringCloudApplication
 public class CustomerApp {
